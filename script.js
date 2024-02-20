67eedc83df74328ad7e97073a6ccf51c0e582fa1
Mahiballah <145888808+mahibullah11@users.noreply.github.com>
2024-02-20 22:52:40 +0500

    done three qustion

---
+++ script.js
@@ -0,0 +1,28 @@
+ // Quesstion No 1  Welcome Message:-------------------------------------------------------------------------->
+
+  // function welcomeMessage(){
+  //   let userName = prompt("Enter Your Name:");
+  //   let result = "welcome"+ " "+ userName + "!";
+  //   console.log(result);
+  // };
+  // welcomeMessage();
+
+
+//  Quesstion No 2 Quote Generator:---------------------------------------------------------------------------------->
+
+// let quote = ["Life is what happens while you are busy making other plans.", "Not all those who wander are lost.", "Nothing is certain except for death and taxes.","“Don't worry about failure; you only have to be right once.","Everything you've ever wanted is sitting on the other side of fear."];
+// function quoteFunction(){
+//   const randomIndex = Math.floor(Math.random() * quote.length);
+//   const randomQut = quote[randomIndex];
+//   console.log(randomQut)
+// }
+// quoteFunction();
+
+ 
+// Quesstion No 3 Random Number Generator:-------------------------------------------------------------------------->
+
+  // function RandomNumber(){
+  //   let num = Math.ceil(Math.random() * 100); 
+  //   console.log(num);
+  // }
+  // RandomNumber();
